7ef38bca59612b4f08da289b145316ad9fe1a55e
ShinYonSung <newxm@naver.com>
2020-05-24 11:26:31 +0900

    별걸 더 만들엇음

--- app/src/main/java/yon/hee/loginpractice_20200524/MainActivity.kt
+++ app/src/main/java/yon/hee/loginpractice_20200524/MainActivity.kt
@@ -2,11 +2,23 @@ package yon.hee.loginpractice_20200524
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            val inputid = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+            Log.d("입력아이디", inputid)
+            Log.d("입력비번", inputPw)
+
+        }
     }
 }
